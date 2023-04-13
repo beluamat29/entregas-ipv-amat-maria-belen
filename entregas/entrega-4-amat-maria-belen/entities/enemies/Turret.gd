cb195bdbc94c23676cf4ff5c142cb0e4c168dd49
mariabelenamat <belen.amat29@gmail.com>
2023-04-13 20:58:09 -0300

    agregando torreta que dispara

--- entregas/entrega-4-amat-maria-belen/entities/enemies/Turret.gd
+++ entregas/entrega-4-amat-maria-belen/entities/enemies/Turret.gd
@@ -1,28 +1,16 @@
 extends Sprite
 
-export (PackedScene) var projectile_scene:PackedScene
 
-onready var fire_position:Position2D = $FirePosition
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
 
-var projectile_container:Node
 
-var player
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
 
-func set_values(player, projectile_container):
-	self.player = player
-	self.projectile_container = projectile_container
-	$Timer.start()
 
-func _on_Timer_timeout():
-	fire()
-	
-func fire():
-	var projectile:Projectile = projectile_scene.instance()
-	projectile_container.add_child(projectile)
-	projectile.set_starting_values(fire_position.global_position, (player.global_position - fire_position.global_position).normalice())
-	projectile.connect("delete_requested", self, "_on_projectile_delete_requested")
-	
-func _on_projectile_delete_requested(projectile):
-		projectile_container.remove_child(projectile)
-		projectile.queue_free()
-
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
